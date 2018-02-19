e836f2c33b76c8040af8da6f04a5b9f981b5e3cc
slmnv5 <slmnv5@gmail.com>
2018-02-18 19:11:31 -0500

    commit 1

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AlsaMapper.cpp \
+../src/MidiEvent.cpp \
+../src/RuleMapper.cpp \
+../src/mimap3.cpp 
+
+OBJS += \
+./src/AlsaMapper.o \
+./src/MidiEvent.o \
+./src/RuleMapper.o \
+./src/mimap3.o 
+
+CPP_DEPS += \
+./src/AlsaMapper.d \
+./src/MidiEvent.d \
+./src/RuleMapper.d \
+./src/mimap3.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -DDEBUG -O0 -g3 -Wall -c   -lasound -H -Winvalid-pch -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
